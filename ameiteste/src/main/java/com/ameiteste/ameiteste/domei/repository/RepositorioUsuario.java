47e883aacb0d0d8644869106ed792cdf8aae74e1
Iago IDev <124836944+IagoSouzas@users.noreply.github.com>
2023-06-26 16:35:22 -0300

    Add files via upload

---
+++ ameiteste/src/main/java/com/ameiteste/ameiteste/domei/repository/RepositorioUsuario.java
@@ -0,0 +1,10 @@
+package com.ameiteste.ameiteste.domei.repository;
+
+import com.ameiteste.ameiteste.domei.model.Usuario;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioUsuario extends CrudRepository<Usuario,Long> {
+
+}
